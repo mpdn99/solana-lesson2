91365ff5d3ceea64081ddf2e7ed62d41ca951b60
mpdn99 <mpdn99@gmail.com>
2022-11-23 18:36:32 +0700

    init

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,30 @@ import { BrowserRouter } from 'react-router-dom'
 
 import View from 'view'
 
+import configs from 'configs'
 import reportWebVitals from 'reportWebVitals'
 
 import 'static/styles/index.less'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { Coin98WalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+
+const {
+  rpc: {endpoint}
+} = configs;
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <View />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider
+       wallets={[new PhantomWalletAdapter(), new Coin98WalletAdapter()]} 
+       autoConnect
+      >
+        <WalletModalProvider>
+          <View />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   </BrowserRouter>,
 )
 
--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,14 +1,28 @@
 import IonIcon from '@sentre/antd-ionicon'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 
 import { Image, Col, Layout, Row, Space, Typography, Button } from 'antd'
 
 import logo from 'static/images/solanaLogo.svg'
+import brand from 'static/images/solanaLogoMark.svg'
 import './index.less'
 
 function View() {
+  const {} = useWallet
   return (
     <Layout className="container">
       <Row gutter={[24, 24]}>
+        <Col span={24}>
+          <Row gutter={[24,24]}>
+            <Col flex="auto">
+              <img alt="logo" src={brand} height={16} />
+            </Col>
+            <Col>
+              <WalletMultiButton/>
+            </Col>
+          </Row>
+        </Col>
         <Col span={24} style={{ textAlign: 'center' }}>
           <Space direction="vertical" size={24}>
             <Image src={logo} preview={false} width={256} />
@@ -22,9 +36,7 @@ function View() {
                 <IonIcon name="rocket" />
               </Space>
             </Typography.Text>
-            <Button type="primary" size="large">
-              Let's go
-            </Button>
+            <Typography.Title>My Balance: {0} SOL</Typography.Title>
           </Space>
         </Col>
       </Row>
